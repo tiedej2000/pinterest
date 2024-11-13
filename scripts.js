621d420e7e167ed24726a3528eb5b52274b6e983
tiedej2000 <tiedejustin1@gmail.com>
2024-11-13 16:59:27 +0100

    sign in modal & section one changes

--- scripts.js
+++ scripts.js
@@ -1,21 +1,71 @@
-// Get modal elements
-const modal = document.getElementById("signupModal");
+/* ------------------- modal ----------------------*/
+
+const modalsignup = document.getElementById("signupModal");
+const modalsignin = document.getElementById("signinModal");
 const closeModal = document.getElementById("closeModal");
 const signUpButton = document.querySelector(".sign-up");
+const signInButton = document.querySelector(".sign-in");
 
-// Show the modal when the sign-up button is clicked
 signUpButton.onclick = function () {
-    modal.style.display = "block";
+    modalsignup.style.display = "block";
 }
 
-// Hide the modal when the close button is clicked
+signInButton.onclick = function () {
+    modalsignin.style.display = "block";
+}
+
 closeModal.onclick = function () {
-    modal.style.display = "none";
+    modalsignup.style.display = "none";
+    modalsignin.style.display = "none";
 }
 
-// Hide the modal when clicking outside the modal content
 window.onclick = function (event) {
-    if (event.target === modal) {
-        modal.style.display = "none";
+    if (event.target === modalsignup) {
+        modalsignup.style.display = "none";
+    }
+    if (event.target === modalsignin) {
+        modalsignin.style.display = "none";
     }
 }
+
+/* ------------------- carousel ----------------------*/
+
+const carouselTexts = [
+    "Abendessen an Wochentagen",
+    "Einrichtungsidee",
+    "Outfit im neuen Look",
+    "Gruener Daumen"
+];
+
+const carouselColor = [
+    "rgb(194, 139, 0)",
+    "rgb(97, 140, 123)",
+    "rgb(0, 118, 211)",
+    "rgb(64, 122, 87)"
+]
+
+function updateCarouselText() {
+    const textElement = document.getElementById("carousel-text");
+    const radios = document.getElementsByName("carousel");
+
+    radios.forEach((radio, index) => {
+        if (radio.checked) {
+            textElement.textContent = carouselTexts[index];
+            textElement.style.color =  carouselColor[index];
+            radio.style.backgroundColor = carouselColor[index];
+        } else{
+            radio.style.backgroundColor = 'lightgray'
+        }
+    });
+}
+
+document.querySelectorAll(".radios input[type='radio']").forEach(radio => {
+    radio.addEventListener("change", updateCarouselText);
+});
+
+updateCarouselText();
+
+
+
+
+
